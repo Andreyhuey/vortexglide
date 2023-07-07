46aee63c85c8549c8b613b348e36a9393121314a
Andreyhuey <andreyhuey777@gmail.com>
2023-07-07 01:26:27 +0100

    work component completed

--- src/components/AboutComp/Work.tsx
+++ src/components/AboutComp/Work.tsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 const Work = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="px-4 lg:px-40 py-16 bg-[#1C140F]">
+        <div>
+          <h6 className="font-semibold text-[25px] py-4">Why work with us</h6>
+          <div className="flex flex-col lg:flex-row justify-center items-center gap-8">
+            <div className="bg-[#473327] rounded-[20px] px-6 py-12 flex flex-col gap-4">
+              <div className="bg-[#B000DC] p-2 rounded-md w-[140px] text-center">
+                Lorem ipsum
+              </div>
+              <p className="font-semibold text-[20px]">Lorem ipsum</p>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
+            </div>
+
+            <div className="bg-[#473327] rounded-[20px] px-6 py-12 flex flex-col gap-4">
+              <div className="bg-[#DC4200] p-2 rounded-md w-[140px] text-center">
+                Lorem ipsum
+              </div>
+              <p className="font-semibold text-[20px]">Lorem ipsum</p>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
+            </div>
+
+            <div className="bg-[#473327] rounded-[20px] px-6 py-12 flex flex-col gap-4">
+              <div className="bg-[#00DC8D] p-2 rounded-md w-[140px] text-center">
+                Lorem ipsum
+              </div>
+              <p className="font-semibold text-[20px]">Lorem ipsum</p>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Work;
