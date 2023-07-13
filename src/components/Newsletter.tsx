3c850cbf86f28fa9a81e94757b6f9c200ddc6ce2
Andreyhuey <andreyhuey777@gmail.com>
2023-07-13 11:11:51 +0100

    Newsletter modified

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -5,7 +5,7 @@ const Newsletter = () => {
   return (
     <>
       <div className="py-32 px-4 lg:px-40 bg-[#1C140F]">
-        <div className="py-6">
+        <div className="pb-6">
           <h6 className="text-[30px] font-bold">Lorem Ipsum</h6>
           <p className="py-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -15,7 +15,7 @@ const Newsletter = () => {
         <div className="lg:p-12 p-3 bg-[#473327] rounded-[20px]">
           <div className="flex justify-between items-center flex-col lg:flex-row gap-4">
             <div>
-              <h6 className="font-medium text-[30px]">Stay in the loop</h6>
+              <h6 className="font-medium text-[30px] ">Stay in the loop</h6>
               <p className="lg:w-[480px]">
                 Subscribe to receive the latest news and updates about TDA. We
                 promise not to spam you!{" "}
@@ -25,7 +25,7 @@ const Newsletter = () => {
               <div className="rounded-xl flex items-center justify-center bg-slate-50 p-1 lg:w-[400px]">
                 <input
                   className="w-full ml-4 focus:outline-none text-black"
-                  placeholder="Enter email address"
+                  placeholder="Email address..."
                 />
                 <button className="rounded-xl px-4 py-2 bg-[#DC7000]">
                   Continue
--- src/components/ServicesComp/Testimonials.tsx
+++ src/components/ServicesComp/Testimonials.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="px-4 lg:px:40 py-8 bg-[#473327]">
+        <div>
+          <div className=" py-5 flex items-center justify-center">
+            <div className="lg:w-[450px] text-center">
+              <p className="text-[35px] font-medium">
+                Trusted by Thousands of Happy Customer
+              </p>
+              <p className="py-4">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.Lorem Ipsum is simply dummy .
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Testimonials;
