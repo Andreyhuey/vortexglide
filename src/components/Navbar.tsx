e2c593e4bcd7e83b5b285fde2d81e18200107652
Andreyhuey <andreyhuey777@gmail.com>
2023-08-28 17:40:00 +0100

    Navbar modified for better ux

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { HiOutlineMenu } from "react-icons/hi";
 import { IoCloseCircleOutline } from "react-icons/io5";
@@ -9,66 +9,75 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
+  // Add this effect to handle body overflow
+  useEffect(() => {
+    if (nav) {
+      document.body.style.overflow = "hidden"; // Prevent scrolling
+    } else {
+      document.body.style.overflow = "auto"; // Allow scrolling
+    }
+    return () => {
+      document.body.style.overflow = "auto"; // Make sure to reset when unmounting
+    };
+  }, [nav]);
+
   return (
     <>
       <div
-        className={`fixed h-auto w-full left-0 top-0 z-30 ${
-          nav ? "bg-black" : "bg-black"
-        } py-5 px-4 lg:px-16`}
+        className={`fixed h-auto w-full left-0 top-0 z-30 bg-black py-5 lg:px-16`}
       >
-        <div>
-          <div className="flex items-center justify-between">
-            <div>
-              <Link className={`${nav ? "opacity-0" : ""}`} href="/">
-                <h2 className="font-[700] text-[24px]">VortexGlide</h2>
+        <div className="flex items-center justify-between px-4">
+          <div>
+            <Link className={`${nav ? "opacity-0" : ""}`} href="/">
+              <h2 className="font-[700] text-[24px]">VortexGlide</h2>
+            </Link>
+          </div>
+
+          {/* for laptops */}
+          <ul className="items-center justify-between hidden lg:flex gap-16">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About us</Link>
+            </li>
+            <li>
+              <Link href="/services">Services</Link>
+            </li>
+            <li>
+              <Link href="/news">News</Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="px-4 py-2 bg-[#FA9021] rounded-md"
+              >
+                Contact us
               </Link>
-            </div>
+            </li>
+          </ul>
 
-            <ul className="items-center justify-between hidden lg:flex gap-16">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/about">About us</Link>
-              </li>
-              <li>
-                <Link href="/services">Services</Link>
-              </li>
-              <li>
-                <Link href="/news">News</Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  className="px-4 py-2 bg-[#FA9021] rounded-md"
-                >
-                  Contact us
-                </Link>
-              </li>
-            </ul>
-
-            <div className="lg:hidden" onClick={handleClick}>
-              {!nav ? (
-                <HiOutlineMenu className="text-[24px]" />
-              ) : (
-                <IoCloseCircleOutline className="text-[24px] right-0 cursor-pointer" />
-              )}
-            </div>
+          {/* for mobile devices and tabs */}
+          <div className="lg:hidden" onClick={handleClick}>
+            {!nav ? (
+              <HiOutlineMenu className="text-[24px]" />
+            ) : (
+              <IoCloseCircleOutline className="text-[24px] right-0 cursor-pointer" />
+            )}
           </div>
         </div>
 
-        {
+        {/* for phones */}
+
+        <div className="w-full">
           <ul
             className={
               !nav
                 ? "hidden"
-                : "absolute h-screen w-full cursor-pointer bg-black"
+                : "absolute h-screen w-full cursor-pointer bg-black py-5"
             }
           >
-            <div className="flex flex-col items-center justify-center gap-16">
-              <li>
-                <h2 className="font-[700] text-[24px]">LOGO</h2>
-              </li>
+            <div className="flex flex-col items-center justify-center gap-16 w-full">
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -91,7 +100,7 @@ const Navbar = () => {
               </li>
             </div>
           </ul>
-        }
+        </div>
       </div>
     </>
   );
